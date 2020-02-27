f8b3cb8bdafd540f42d95d2dfc28754460eadb8d
notreniev <everton.canez@gmail.com>
2020-02-27 16:43:16 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/navbar/NavBar';
+import Header from './components/header/Header';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col sm="12">
+            <Header />
+          </Col>
+        </Row>
+        <Row>
+          <Col sm="12">
+            <NavBar />
+          </Col>
+          <Col sm="12">
+            content area
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
